32e8185d53edceb2079d47ef98426fb2b67094f6
Muhammad Sarim <36129452+muhammadsarim555@users.noreply.github.com>
2023-02-18 03:18:58 +0500

    Add marksheet assignment 1

---
+++ marksheet.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+input("Enter your name")
+
+
+# In[2]:
+
+
+print("----------------------------------------------------------------------------------")
+
+
+# In[30]:
+
+
+studentName = input("Enter your name!");
+fName = input("Enter your Father name!")
+# phy = input("Enter Physics Number");
+# math = input("Enter Math Number");
+# che = input("Enter Chemistry Number");
+print("-------------------------------------------------------------------------------------------"
+      "----\n\n\n\nName:\t" 
+      + studentName +"\nFather Name:   " + fName + "\n\t\t\t\t\t   MARKSHEET"
+      
+    )
+print("---------------------------------------------- | ------------------------------------------------")
+print("Physics:\t\t\t\t\t\t\t\t 70")
+print("---------------------------------------------- | ------------------------------------------------")
+print("Maths:\t\t\t\t\t\t\t\t\t 70")
+print("---------------------------------------------- | ------------------------------------------------")
+print("Chemistry:\t\t\t\t\t\t\t\t 70")
+print("\n\t\t\t\t\t\t\tMarks\t:\t222")
+print("\n\t\t\t\t\t\t\tTotal Marks:\t 300")
+print("\n\t\t\t\t\t\t\tPercentage:\t 78.00")
+print("\n\t\t\t\t\t\t\tGrade\t:\t A")
+print("\n\t\t\t\t\t\t\tResult\t:\tPass")
+print("\n\n\n\n\n--------------------------------------------------------------------------------------------------")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
